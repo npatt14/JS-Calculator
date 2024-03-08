9281a95b58d5c210e2f4f964937608df5cf65654
Nathan Patterson <imnathanpatterson@gmail.com>
2024-03-08 10:59:35 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+// MANIPULATE DISPLAY ELEMENT 
+const display = document.getElementById("display")
+
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch(error) {
+        display.value = "error";
+    }
+    
+}
+
